2f4d2dcfb55b399a062f26ea593aec85ee91bc0d
Frankebuka <frankebuka@yahoo.com>
2023-10-26 10:57:31 +0000

    create sign up api route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  await newUser
+    .save()
+    .then(() => {
+      res.status(201).json("User created!");
+    })
+    .catch((err) => {
+      res.status(400).json("Error: " + err.message);
+    });
+};
