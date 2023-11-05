5a906e795c41193a44721c77e58ec5683ff29810
Frankebuka <frankebuka@yahoo.com>
2023-11-05 12:02:36 +0000

    add onChange and onSubmit functionality to the search page

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,8 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const navigate = useNavigate();
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+
+  console.log(listings);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+
+    const fetchListings = async () => {
+      setLoading(true);
+      setShowMore(false);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
+      setListings(data);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "rent" ||
+      e.target.id === "sale"
+    ) {
+      setSidebardata({ ...sidebardata, type: e.target.id });
+    }
+
+    if (e.target.id === "searchTerm") {
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+
+      const order = e.target.value.split("_")[1] || "desc";
+
+      setSidebardata({ ...sidebardata, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -17,7 +128,7 @@ const Search = () => {
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
-              //   value={sidebardata.searchTerm}
+              value={sidebardata.searchTerm}
               onChange={handleChange}
             />
           </div>
@@ -29,7 +140,7 @@ const Search = () => {
                 id="all"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.type === "all"}
+                checked={sidebardata.type === "all"}
               />
               <span>Rent & Sale</span>
             </div>
@@ -39,7 +150,7 @@ const Search = () => {
                 id="rent"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.type === "rent"}
+                checked={sidebardata.type === "rent"}
               />
               <span>Rent</span>
             </div>
@@ -49,7 +160,7 @@ const Search = () => {
                 id="sale"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.type === "sale"}
+                checked={sidebardata.type === "sale"}
               />
               <span>Sale</span>
             </div>
@@ -59,7 +170,7 @@ const Search = () => {
                 id="offer"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.offer}
+                checked={sidebardata.offer}
               />
               <span>Offer</span>
             </div>
@@ -72,7 +183,7 @@ const Search = () => {
                 id="parking"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.parking}
+                checked={sidebardata.parking}
               />
               <span>Parking</span>
             </div>
@@ -82,7 +193,7 @@ const Search = () => {
                 id="furnished"
                 className="w-5"
                 onChange={handleChange}
-                // checked={sidebardata.furnished}
+                checked={sidebardata.furnished}
               />
               <span>Furnished</span>
             </div>
